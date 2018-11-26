8fc9f1175247d9b38aba2479acacb365696dc521
Bence Kővári <bkovari93@gmail.com>
2018-11-26 20:55:19 +0100

    CHG: startCluster renamed, main finalized

--- src/main/java/com/amazonaws/batchdataingestion/BatchDataIngestion.java
+++ src/main/java/com/amazonaws/batchdataingestion/BatchDataIngestion.java
@@ -4,24 +4,23 @@ import java.util.Arrays;
 
 public class BatchDataIngestion {
 
-	private EMRCluster startCluster() {
+	private EMRCluster getConfiguredCluster() {
 
 		EMRCluster cluster = new EMRCluster("MyAWSCluster", "eu-central-1");
 		cluster.setApplications("emr-5.17.0", Arrays.asList("Sqoop", "Hadoop", "Hive"));
 		cluster.setBootstrapConfig("s3://batchdataingestion/scripts/bootstrap.sh");
 		cluster.setMasterNode("m4.xlarge");
 		cluster.setCoreNode("m4.large", 2);
-		cluster.start(true);
-
 		return cluster;
-
 	}
 
 	public static void main(String[] args) {
-
 		BatchDataIngestion dataIngestionApplication = new BatchDataIngestion();
-		EMRCluster cluster = dataIngestionApplication.startCluster();
-		// cluster.addJob("Test", "s3://batchdataingestion/scripts/job.sh");
-	}
+		EMRCluster cluster = dataIngestionApplication.getConfiguredCluster();
 
+		if (!EMRCluster.isClusterRunning(cluster.getName(), cluster.getRegion())) {
+			cluster.start(true);
+			cluster.addJob("OracleIngestion", "s3://batchdataingestion/scripts/hive_oracle_ingestion.sh");
+		}
+	}
 }
